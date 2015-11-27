6b0301e19a733760a1e58332c5943bfa9f9c9a5e
SimonLab <simon.labondance@gmail.com>
2015-11-27 15:39:11 +0000

    play with click

--- chrome-extension/browser_action/js/main.js
+++ chrome-extension/browser_action/js/main.js
@@ -1,6 +1,21 @@
 (function () {
-  var tigerButton = document.getElementById('tiger-button');
-  tigerButton.addEventListener('click', function (e) {
-    alert('Miaou');
-  })
-}());
+
+  var incrButton = document.getElementsByClassName('incr-button')[0];
+    incrButton.addEventListener('click', function (e){
+      e.preventDefault();
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', 'https://simplegm.herokuapp.com/click');
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          if (xhr.responseText === '200') {
+            console.log('yeaaaa')
+          }
+          console.log(xhr.responseText);
+        }
+      };
+
+      xhr.send(null);
+    }, false);
+
+})();
